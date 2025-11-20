29f09836d5bf2a6c4e5ede9570673f89270a8a7c
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-11-20 19:21:05 +0545

    vault backup: 2025-11-20 19:21:05

--- SEM I/Programming in C - 115/test.c
+++ SEM I/Programming in C - 115/test.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
-void main(){
-    char a[25] = 'Hello';
-    char b[25] = 'World';
-    printf("%s %s\n",a,b);
+#include <stdio.h>
+
+int main() {
+    char a[] = "Hello";  // The empty [] let's compiler determine size
+    char b[] = "World";
+    printf("%s %s\n", a, b);
+    return 0;
 }
