cfbc51c8b48f6c82d656643e48eb5e65997dc750
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-11-20 19:22:28 +0545

    vault backup: 2025-11-20 19:22:28

--- SEM I/Programming in C - 115/test.c
+++ SEM I/Programming in C - 115/test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*  This is just a placeholder program. May be removed later.*/
 int main() {
     char a[] = "Hello";  // The empty [] let's compiler determine size
     char b[] = "World";
