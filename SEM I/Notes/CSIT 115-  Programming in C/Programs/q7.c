9dc8f91fe68d614676c075cc900304d71af24daa
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-11-29 19:45:17 +0545

    vault backup: 2025-11-29 19:45:17

--- SEM I/Notes/CSIT 115-  Programming in C/Programs/q7.c
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/q7.c
@@ -3,10 +3,8 @@
 
 int main(){
     int a, b;
-    printf("Enter first integer: ");
-    scanf("%d", &a);
-    printf("Enter second integer: ");
-    scanf("%d", &b);
+    printf("Enter the two integer: ");
+    scanf("%d%d", &a,&b);
     printf("Relational Operations between %d and %d:\n", a, b);
     printf("%d > %d : %s\n", a, b, (a > b) ? "True" : "False"); /* This statement basically means: if(a>b){printf("true");}
                                                                                                     else {printf("false");} */
