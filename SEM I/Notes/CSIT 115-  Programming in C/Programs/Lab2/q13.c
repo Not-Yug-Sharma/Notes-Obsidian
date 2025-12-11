51209ffbfce4a427030d48b9db74cd42d78fffe9
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 17:36:41 +0545

    vault backup: 2025-12-11 17:36:41

---
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q13.c
@@ -0,0 +1,23 @@
+// Program to check if a number is palindrome
+#include<stdio.h>
+
+int main(){
+    int n, original, reversed = 0, digit;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+
+    while (n != 0){
+        digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reversed){
+        printf("%d is a palindrome.\n", original);
+    }
+    else{
+        printf("%d is not a palindrome.\n", original);
+    }
+    return 0;
+}
