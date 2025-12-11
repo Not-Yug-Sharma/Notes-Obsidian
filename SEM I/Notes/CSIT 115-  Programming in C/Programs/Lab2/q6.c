d9e58ecf2c2c4209a9f3fc42fa488135aaa2256f
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 17:17:51 +0545

    vault backup: 2025-12-11 17:17:51

--- SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q6.c
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q6.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 
 int main(){
-    int marks[5], total = 0;
+    float marks[5], total = 0;
     float percentage;
     printf("Enter marks obtained in 5 subjects: ");
     for (int i = 0; i < 5; i++){
@@ -13,23 +13,20 @@ int main(){
     percentage = (total / 500.0) * 100;
     printf("Total Marks: %d/500\n", total);
     printf("Percentage: %.2f%%\n", percentage);
-    switch((int)(percentage / 10)){
-        case 10:
-        case 9:
-            printf("Division: First Division\n");
-            break;
-        case 8:
-         printf("Division: First Division\n");
-            break;
-        case 7:
-            printf("Division: Second Division\n");
-            break;
-        case 6:
-        case 5:
-            printf("Division: Third Division\n");
-            break;
-        default:
-            printf("Fail\n");
+    if (percentage >= 80){
+        printf("Division: Distinction\n");
+    }
+    else if (percentage >= 70){
+        printf("Division: First Division\n");
+    }
+    else if (percentage >= 60){
+        printf("Division: Second Division\n");
+    }
+    else if (percentage >= 50){
+        printf("Division: Third Division\n");
+    }
+    else{
+        printf("Result: Fail\n");
     }
     return 0;
 }
