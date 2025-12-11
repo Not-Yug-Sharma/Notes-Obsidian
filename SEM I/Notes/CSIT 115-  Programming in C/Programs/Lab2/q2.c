379bb11e535727f2ace588d6353deb42b6413fc3
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 15:46:41 +0545

    vault backup: 2025-12-11 15:46:41

---
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q2.c
@@ -0,0 +1,13 @@
+// Progrsm to demonstrate the use of conditional (ternary) operator
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("The greater number between %d and %d is %d\n", a, b, (a > b) ? a : b);
+    printf("\nIs %d smaller than %d? %c\n", a, b, (a < b) ? "Y" : "N");
+    return 0;
+}
