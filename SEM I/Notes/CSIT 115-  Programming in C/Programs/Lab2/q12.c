551cc9d306a78e8bb29870ee681003b36d26686a
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 17:34:23 +0545

    vault backup: 2025-12-11 17:34:23

--- SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q12.c
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q12.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 
 int main(){
-    int sum=0;
+    double sum=0;
     double n;
     printf("Enter a number: ");
     scanf("%lf", &n);
     while(n!=0){
-        sum += (int)n % 10; // sum = sum + (n % 10);
-        n = (int)n / 10;
+        sum += n % 10; // sum = sum + (n % 10);
+        n = n / 10;
     }
-    printf("Sum of digits: %d\n", sum);
+    printf("Sum of digits: %lf\n", sum);
     return 0;
 }
