3e2e1d2621db3a80efb379ea319abf0decac1510
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 17:27:44 +0545

    vault backup: 2025-12-11 17:27:44

--- SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q8.c
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q8.c
@@ -6,13 +6,13 @@ int main(){
     printf("Enter three float numbers: ");
     scanf("%f %f %f", &a, &b, &c);
     if (a >= b && a >= c){
-        printf("%.2f is the largest number.\n", a);
+        printf("%f is the largest number.\n", a);
     }
     else if (b >= a && b >= c){
-        printf("%.2f is the largest number.\n", b);
+        printf("%f is the largest number.\n", b);
     }
     else{
-        printf("%.2f is the largest number.\n", c);
+        printf("%f is the largest number.\n", c);
     }
     return 0;
 }
