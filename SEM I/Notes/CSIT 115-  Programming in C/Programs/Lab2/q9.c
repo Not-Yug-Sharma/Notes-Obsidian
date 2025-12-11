cd0f2aa8fe947efba08f828972e5f5dc6a763d16
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-11 17:29:21 +0545

    vault backup: 2025-12-11 17:29:21

---
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab2/q9.c
@@ -0,0 +1,14 @@
+// Program to pront multiplication table
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter an integer to print its multiplication table: ");
+    scanf("%d", &n);
+
+    printf("Multiplication table of %d:\n", n);
+    for (int i = 1; i <= 10; i++){
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+    return 0;
+}
