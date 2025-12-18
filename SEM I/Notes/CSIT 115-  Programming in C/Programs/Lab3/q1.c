1b6968cca84506c79aeb1ad292fab25e18ee2a8c
YugSTimilsina <ysharmatimilsina@gmail.com>
2025-12-18 16:40:40 +0545

    vault backup: 2025-12-18 16:40:40

---
+++ SEM I/Notes/CSIT 115-  Programming in C/Programs/Lab3/q1.c
@@ -0,0 +1,26 @@
+//WAP to input any integer number and check it either it is prime or not.
+
+#include <stdio.h>
+
+
+int main(){
+    int num, factor=0;
+    printf("Enter an integer number: ");
+    scanf("%d", &num);
+    for (int i=1; i<=num;i++){
+        if (num %i==0)
+            factor++;
+    }
+    if (num<=1){
+        printf("The number is not prime.");
+    }
+    else if(num=1)
+        printf("Neither Prime nor Composite.");
+  
+    else if (factor==2)
+        printf("The number is prime.");
+    else
+        printf("The number is not prime.");
+
+        return 0;
+}
